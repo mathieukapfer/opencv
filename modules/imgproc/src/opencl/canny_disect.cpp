485448c23efd6690e018de4574d547b3e4334728
Mathieu Kapfer <mkapfer@kalrayinc.com>
2021-01-08 19:50:32 +0100

    [doc] add compute mag analyse

--- modules/imgproc/src/opencl/canny_disect.cpp
+++ modules/imgproc/src/opencl/canny_disect.cpp
@@ -2,6 +2,7 @@
 
 #define MAX(a, b) a > b ? a : b
 #define MIN(a, b) a < b ? a : b
+#define min(a, b) a < b ? a : b
 #define clamp(val, min, max) MAX(MIN(val,max),min)
 
 #define GRP_SIZEY 4
@@ -10,11 +11,11 @@
 #define cols 10
 #define rows 10
 
-int src[GRP_SIZEX*cols + GRP_SIZEY*rows];
+int src[cols * rows];
 int smem[(GRP_SIZEX + 4) * (GRP_SIZEY + 4)];
 
 #define src_offset 0
-#define src_step GRP_SIZEX *cols
+#define src_step cols
 #define TYPE char
 
 const int cn = 1;
@@ -36,19 +37,48 @@ void loadpixloop(int lidx, int lidy, int start_x, int start_y) {
     int x = clamp(start_x - 2 + (j % (GRP_SIZEX + 4)), 0, cols - 1);
     int y = clamp(start_y - 2 + (j / (GRP_SIZEX + 4)), 0, rows - 1);
 
-    printf("smem[%d] = src[%d]  ",j, mad24(y, src_step, mad24(x, cn * (int)sizeof(TYPE), src_offset)));
+    printf("smem[%d] = loadpix(%d)  ",j, mad24(y, src_step, mad24(x, cn * (int)sizeof(TYPE), src_offset)));
     //smem[j] = loadpix(
     //    src + mad24(y, src_step, mad24(x, cn * (int)sizeof(TYPE), src_offset)));
   }
 }
 
+void compute_mag(int lidx, int lidy, int start_x, int start_y)
+{
+    int i = lidx + lidy * GRP_SIZEX;
+
+    lidx++;
+    lidy++;
+
+    if (i < GRP_SIZEX + 2)
+    {
+        int grp_sizey = min(GRP_SIZEY + 1, rows - start_y);
+        printf("mag[%2d] = sobel(%2d)  ", i, i);
+        printf("mag[%2d] = sobel(%2d)  ",
+               i + grp_sizey * (GRP_SIZEX + 2),
+               i + grp_sizey * (GRP_SIZEX + 4));
+    }
+    if (i < GRP_SIZEY + 2)
+    {
+        int grp_sizex = min(GRP_SIZEX + 1, cols - start_x);
+        printf("mag[%2d] = sobel(%2d)  ", i * (GRP_SIZEX + 2), i * (GRP_SIZEX + 4));
+        printf("mag[%2d] = sobel(%2d)  ", i * (GRP_SIZEX + 2) + grp_sizex, i * (GRP_SIZEX + 4) + grp_sizex);
+    }
+
+    int idx = lidx + lidy * (GRP_SIZEX + 4);
+    i = lidx + lidy * (GRP_SIZEX + 2);
+
+    printf("mag[%2d] = sobel(%2d) ", i, idx);
+
+}
 int main(int argc, char *argv[]) {
 
   for (int y=0; y<GRP_SIZEY;y++)
     for (int x=0; x<GRP_SIZEX; x++)
       {
-        printf("\nlidx:%d, lidy:%d =>", x,y);
-        loadpixloop(x,y,2,2);
+        printf("\nlidx:%2d, lidy:%2d =>", x,y);
+        //loadpixloop(x,y,2,2);
+        compute_mag(x,y, 2, 2);
       }
 
   return 0;
