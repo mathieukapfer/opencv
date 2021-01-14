f3342ade75767b5b39eb650b3ebb6c0a039032bc
Kapfer Family <kapfer@debian-desktop>
2021-01-14 07:42:07 +0100

    [parser] get kernel detail from test report

--- format_report.el
+++ format_report.el
@@ -2,19 +2,43 @@
 
 
 ;;; Commentary:
-;;
+
 
 ;;; Code:
+
+;; Const
+(setq num-regexp "[0-9]+")
+(setq name-regexp "[A-Za-z0-9_.]+")
+(setq name-with-point-regexp "[A-Za-z0-9_.]+")
+
+(setq test-name-regexp (concat "RUN.*" "\\(" "OCL" name-with-point-regexp "/" num-regexp "\\)") )
+(setq kernel-name-regexp (concat "clEnqueueNDRangeKernel('" "\\("  name-regexp "\\)" "'" ))
+
 
 (defun my-format-perf-report()
   (interactive)
   (goto-char 1)
   (setq output (get-buffer-create "*perf-report*"))
-  (while (search-forward-regexp "RUN.*\\(OCL[A-Za-z0-9_.]*/[0-9]+\\)" nil t)
-    (setq test-name (match-string 1))
-    (search-forward-regexp "mean=\\([0-9.]+\\)")
-    (setq test-mean (match-string 1))
-    (princ (format "%s: %s\n" test-name test-mean) output)
+  (while
+      (search-forward-regexp (concat kernel-name-regexp  "\\|" test-name-regexp ) nil t)
+    (goto-char (match-beginning 0))
+
+    (let ((data (match-data)))
+      (cond
+       ((looking-at test-name-regexp)
+        (setq test-name (match-string 1))
+        (search-forward-regexp "mean=\\([0-9.]+\\)")
+        (setq test-mean (match-string 1))
+        (princ (format "\n%s: %s\n" test-name test-mean) output)
+        )
+       ((looking-at kernel-name-regexp)
+        (setq kernel-name (match-string 1))
+        (princ (format " %s " kernel-name) output)
+        )
+       )
+      (set-match-data data)
+      (goto-char (match-end 0))
+      )
     )
   (switch-to-buffer output)
   )
